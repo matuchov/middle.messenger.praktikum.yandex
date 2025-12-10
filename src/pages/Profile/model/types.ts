17428444da6534ec987d954c2428c20220bf8185
Matuchov <matuchov@gmail.com>
2025-12-10 22:09:39 +0300

    Profile refactor

--- src/pages/Profile/model/types.ts
+++ src/pages/Profile/model/types.ts
@@ -15,4 +15,14 @@ export interface ProfileProps extends defaultProps {
   sumbitBtn?: MyButtonBlock;
   formContent?: Form;
   avatarComponent?: MyLink;
+  user?: {
+    avatar: string;
+    display_name: string;
+    email: string;
+    first_name: string;
+    id: number;
+    login: string;
+    phone: string;
+    second_name: string;
+  };
 }
